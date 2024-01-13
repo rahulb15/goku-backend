67ee16bfbd36fb26ea783dff54d6a0fbfa89ee0f
rahul <gamefever91@live.com>
2024-01-14 01:18:01 +0530

    change 1

--- src/helpers/email.helper.js
+++ src/helpers/email.helper.js
@@ -6,16 +6,6 @@ var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi()
 // Configure API key authorization: apiKey
 
 apiInstance.setApiKey(SibApiV3Sdk.AccountApiApiKeys.apiKey,"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")
-// var defaultClient = SibApiV3Sdk.ApiClient.instance;
-
-// // Configure API key authorization: api-key
-// var apiKey = defaultClient.authentications['api-key'];
-// apiKey.apiKey = process.env.API_KEY;
-
-// Uncomment below two lines to configure authorization using: partner-key
-// var partnerKey = defaultClient.authentications['partner-key'];
-// partnerKey.apiKey = 'YOUR API KEY';
-
 
 
 
